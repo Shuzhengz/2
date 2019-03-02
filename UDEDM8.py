65ad24ac0d3cf09c4e1e4ec20f5f3a18224ee655
Tom Zhang <treez.zhang@gmail.com>
2019-03-02 11:30:11 -0800

    Bug fix Fixed a bug where error appears after entering 0

--- UDEDM8.py
+++ UDEDM8.py
@@ -2,15 +2,21 @@
 #CC from Milo Kerr
 from random import randint
 y = int(input("Player Number: "))
-p = int (input("Chance of dying (1 in something): "))
+while y == 0:
+    print("There must be at least one player!")
+    y = int(input("Enter again: "))
+p = int(input("Chance of dying (1 in something): "))
+while p == 0:
+    print("Chance of dying cannot be zero!")
+    p = int(input("Enter again: "))
 x = y
 while y != 0:
 	z = randint (1, p)
 	if y == 1:
 		break
 	if z  ==  1:
-		print ("UDEDM8")
+		print ("UDEDM8 (" + str(randint(1 , x)) + ")")
 		y = y - 1
 	else:
 		print ("UAIGHT")
-print ("Player " + str(randint (1, x )) + " wins!")
+print ("Player " + str(randint(1 , x )) + " wins!")
